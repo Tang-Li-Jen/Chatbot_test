626d47794ceb542f332ba03f3348ad753b8d9191
Tang-Li-Jen <b00701160@gmail.com>
2017-02-18 23:12:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,8 +38,11 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     if messaging_event["message"].get("text"):
-                        message_text = messaging_event["message"]["text"]  # the message's text
-                        send_message(sender_id, message_text)
+                        try:
+                            message_text = messaging_event["message"]["text"]  # the message's text
+                            send_message(sender_id, message_text)
+                        except UnicodeEncodeError:
+                            pass
                     else:
                         #message_text ='Opps, no picture!'
                         send_image(sender_id)
